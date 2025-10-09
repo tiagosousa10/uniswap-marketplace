cd8de5d53cf403a9e9f5b427262678508a915973
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-10-09 18:38:16 +0100

    create custome token erc20 contract to mint and getbalance and total supply for the custom dex

--- deploy/contracts/CustomEx.sol
+++ deploy/contracts/CustomEx.sol
@@ -1,3 +1,50 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.0;
+
+import @openzeppelin/contracts/token/ERC20/ERC20.sol;
+
+contract CustomToken is ERC20 {
+   contructor(string memory name, string memory symbol) ERC20 (name, symbol) {
+      _mint(msg.sender, 10000000 * 10**18);
+   }
+}
+
+contract CustomDex {
+   //custom tokens to be initialized
+   string [] public tokens = ["Tether USD", "BNB", "USD Coin" ,"stETH", "TRON", "Matic Token", "SHIBA INU", "Uniswap"];
+
+   // map to maintain the tokens and its instances
+   mapping(string => ERC20) public tokenInstanceMap;
+
+   uint256 public ethValue = 100000000000000;
+
+   struct History {
+      uint256 historyId;
+      string tokenA;
+      string tokenB;
+      uint256 inputValue;
+      uint256 outputValue;
+      address userAddress;
+
+   }
+
+   uint256 public _historyIndex;
+   mapping(uint256 => History) public historys;
+
+   constructor() {
+      for(uint i=0; i< tokens.length; i++) {
+         CustomToken token = new CustomToken(tokens[i], tokens[i]);
+         tokenInstanceMap[tokens[i]] = token;
+      }
+
+   }
+
+   function getBalance(string memory tokenName,address _address) public view returns (uint256)  {
+      return tokenInstanceMap[tokenName].balanceOf(_address);
+   }
+
+   function getTotalSupply(string memory tokenName) public view returns(uint256) {
+      return tokenInstanceMap[tokenName].totalSupply();
+   }
+}
