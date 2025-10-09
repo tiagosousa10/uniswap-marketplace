7e6a458d35eab796127b69b9d7d1fec805ab52a5
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-10-09 18:46:59 +0100

    transaction history and swap eth to token functionalities

--- deploy/contracts/CustomEx.sol
+++ deploy/contracts/CustomEx.sol
@@ -59,4 +59,29 @@ contract CustomDex {
    function getEthBalance() public view returns(uint256) {
       return address(this).balance;
    }
+
+   function _transactionHistory(string memory tokenName, string memory etherToken, uint256 inputValue, uint256 outputValue) internal {
+      _historyIndex++;
+      uint256 _historyId = _historyIndex;
+      History storage history = historys[_historyId];
+      
+      history.historyId = _historyId;
+      history.userAddress = msg.sender;
+      history.tokenA = tokenName;
+      history.tokenB = etherToken;
+      history.inputValue = inputValue;
+      history.outputValue = outputValue;
+   }
+
+   function swapEthToToken(string memory tokenName) public payable returns(uint256) {
+      uint256 inputValue = msg.value; 
+      uint256 outputValue = (inputValue / ethValue) * 10 ** 18; //convert to 18 decimal places
+
+      require(tokenInstanceMap[tokenName].transfer(msg.sender, outputValue));
+
+      string memory etherToken = "Ether";
+
+      _transactionHistory(tokenName, etherToken, inputValue, outputValue);
+      return outputValue;
+   }
 }
